20b3561913dc46847775edafa6465bc339ea7c39
uriah harston <uharston@gmail.com>
2025-12-25 14:22:04 -0500

    feat: installs sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,34 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
-	},
-	site: "https://mintaka.co",
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "it"],
-	},
-	markdown: {
-		drafts: true,
-		shikiConfig: {
-			theme: "css-variables",
-		},
-	},
-	shikiConfig: {
-		wrap: true,
-		skipInline: false,
-		drafts: true,
-	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		mdx(),
-		icon(),
-	],
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+    },
+    site: "https://mintaka.co",
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "it"],
+    },
+    markdown: {
+        drafts: true,
+        shikiConfig: {
+            theme: "css-variables",
+        },
+    },
+    shikiConfig: {
+        wrap: true,
+        skipInline: false,
+        drafts: true,
+    },
+    integrations: [tailwind({
+        applyBaseStyles: false,
+		}), sitemap(), mdx(), icon(), sanity()],
 });
